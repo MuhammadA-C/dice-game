f301705db76a5259938504fc163faa7d0f712f8a
MuhammadA-C <phantomganhdi@gmail.com>
2023-07-26 16:04:02 -0700

    Button Bug
    Found a bug where if I set the setTimeout() function to a longer time then the user will be able to click the button multiple times.
    
    Then once the setTimeout() function is done there will be a delay and all those delayed setTimeout() functions will trigger.
    
    Fix: Added a boolean check to see if this event listener was triggered and if so break out of the function.

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,33 @@
 const btn = document.querySelector("#btn");
+let wasTriggered = false;
 
 btn.addEventListener("click", () => {
-  console.log("Button pressed");
-  btnHoverEffectOff();
+  //Checks if the event listner was triggered before it was supposed to
+  if (wasTriggered) {
+    return;
+  }
 
   const delayInMiliSeconds = 100;
+  wasTriggered = true;
+
+  console.log("Button pressed");
+
+  btnHoverEffectOff();
   setTimeout(btnHoverEffectOff, delayInMiliSeconds);
+  //Need to reset the boolean so this event listner can be triggered
+  setTimeout(resetWasTriggered, delayInMiliSeconds);
 });
 
 function btnHoverEffectOff() {
   btn.classList.toggle("hover-effect");
 }
+
+function resetWasTriggered() {
+  wasTriggered = false;
+}
+
+/* Note: Will need to add the code that runs the game into a function,
+  then pass that function as a parameter into the setTimeout() funciton
+  to time the delay of another game playing with when the button toggle 
+  effect is added back
+*/
